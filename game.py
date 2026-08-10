ad45255135463f9d819474810b5d10be03c34698
Eddie Huang <184601@cloud.rangitoto.school.nz>
2026-08-10 21:58:59 +0000

    spelling bee in development

--- game.py
+++ game.py
@@ -2,11 +2,21 @@ number = input("You are in a maze pick a path 1-3: ")
 
 if number == "1":
     print("You went left")
-    choice = input("Pick a door 1-2: ")
+    choice = input("There is two heavy doors, pick 1-2: ")
     if choice == "1":
         print("You came across a hole and fell, congratulations!")
     elif choice == "2":
         print("There is a straight path, do you want to walk through it?")
-        answer = input("Yes or No: ")
-        if answer == "Yes":
+        answer = input("yes or no: ")
+        if answer == "yes":
             print("You walked on and on hoping for an exit but you ended up dying from physical overextertion.")
+        elif answer == "no":
+            print("You are now stuck forever because the door trapped you in.")
+
+elif number == "2":
+    print("You went straight")
+    choice = input("left or right: ")
+    if choice == "left":
+        print("There is only one correct path, good for you, this isn't the correct one, you died to a trap")
+    elif choice == "right":
+        print("")
---
+++ spellingbee.py
@@ -0,0 +1,8 @@
+print("Welcome to guess the word")
+print("Hint: A long yellow fruit")
+spelling = input("Your guess: ")
+if spelling == "banana":
+    print("Correct!")
+else:
+    spelling != "banana"
+    print("Incorrect!")
