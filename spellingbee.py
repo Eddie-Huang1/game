42beef9ea19518b7162163bc144665d510f80031
Eddie Huang <184601@cloud.rangitoto.school.nz>
2026-08-11 23:03:01 +0000

    finished basic coding for spelling bee game

--- spellingbee.py
+++ spellingbee.py
@@ -1,8 +1,29 @@
-print("Welcome to guess the word")
+print("Welcome to spelling bee")
+print("--- Round 1 of 3 ---")
 print("Hint: A long yellow fruit")
 spelling = input("Your guess: ")
 if spelling == "banana":
     print("Correct!")
 else:
     spelling != "banana"
-    print("Incorrect!")
+    print("Incorrect, the answer is banana")
+
+print("--- Round 2 of 3 ---")
+print("Hint: A food that comes in a square box, has the shape of a circle and is cut in triangles")
+spelling = input("Your guess: ")
+if spelling == "pizza":
+    print("Correct!")
+else:
+    spelling != "pizza"
+    print("Incorrect, the answer is pizza")
+
+print("--- Round 3 of 3 ---")
+print("Hint: Supermans weakness")
+spelling = input("Your guess: ")
+if spelling == "kryptonite":
+    print("Correct!")
+else:
+    spelling != "kryptonite"
+    print("Incorrect, the answer is kryptonite")
+
+print("--- GAME OVER ---")
